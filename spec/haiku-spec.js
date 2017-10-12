18b946ee296e74b8ceadd53012df496ea087564c
Karla Buckner and Nate McGregor <nate@>
2017-10-12 16:37:44 -0700

    add specs and pass tests

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -5,12 +5,20 @@ describe('Word', function() {
   let twoword;
   let oneword;
   let eword;
+  let ireWord;
+  let leWord;
+  let smallWord;
+  let threeVowel;
 
   beforeEach(function() {
     threeword = new Word("dividing");
     twoword = new Word("cooking");
     oneword = new Word("go");
     eword = new Word("gone");
+    ireWord = new Word("tire");
+    leWord = new Word("pickle");
+    smallWord = new Word("are");
+    threeVowel = new Word("gracious")
   });
 
   it('checks how many syllables are in a word', function() {
@@ -22,6 +30,19 @@ describe('Word', function() {
   it('subtracts a syllable when a word ends with a silent e', function() {
     expect(threeword.countSyllables()).toEqual(3);
     expect(eword.countSyllables()).toEqual(1);
+  });
+
+  it('does not subtract a syllable if word ends with [ire] ', function() {
+    expect(ireWord.countSyllables()).toEqual(2);
+  });
+  it('does not subtract a syllable if word ends with  [le]', function() {
+    expect(leWord.countSyllables()).toEqual(2);
+  });
+  it('does not subtract syllable when word under 3 characters starts with a vowel and ends with an e', function() {
+    expect(smallWord.countSyllables()).toEqual(1);
+  });
+  it('subtracts 2 syllables when word has 3 vowels in a row', function() {
+    expect(threeVowel.countSyllables()).toEqual(2);
   });
 });
 
